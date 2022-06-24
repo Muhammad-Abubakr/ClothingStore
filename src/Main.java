fc818ab85d1c011b2f124be3d6bf6fe1235263ee
frostywolf <kmabs36@gmail.com>
2022-06-24 13:47:01 +0500

    Established Connection with the local database and successfully querying the data from the local Oracle Database

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+// importing the required Classes for establishing connection with OracleDB
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, friend! \nTime to set up Oracle Database...");
+        System.out.println("hello, friend! \nSetting up Oracle Database...");
         // Commit : added jar driver and local host as data source
+
+        // Creating a Connection Class Object
+        Connection con;
+
+        try {
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+            System.out.println("Connection Successfully Established...!");
+
+            System.out.println("\nExecuting Query : \"SELECT * FROM EMPLOYEES\" ");
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery("SELECT * FROM EMPLOYEES");
+
+            int row = 1;
+            while(result.next()) {
+                System.out.println("Row " + row++ + " ==> " +
+                        result.getInt(1) + ", " +
+                        result.getString(2) + ", " +
+                        result.getString(3) + ", " +
+                        result.getString(4) + ", " +
+                        result.getString(5) + ", " +
+                        result.getDate(6) + " "
+                        );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
     }
 }
