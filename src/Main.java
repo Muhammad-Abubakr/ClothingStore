5e91b87e8b83df629f12cb0721a204d5d729de6e
frostywolf <kmabs36@gmail.com>
2022-06-24 18:12:23 +0500

    Oracle Connection Code extracted from main and encapsulated under package database in Oracle.java file Now when using the Oracle database, an object must be instantiated from the class which will automatically initialize the database with the hardcoded arguments to DriverManager.getConnection(hostname, username, password)

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,15 @@
 // importing the required Classes for establishing connection with OracleDB
+import database.Oracle;
+
+import java.io.File;
 import java.sql.*;
+import java.time.Instant;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, friend! \nSetting up Oracle Database...");
-        // Commit : added jar driver and local host as data source
 
-        // Creating a Connection Class Object
-        Connection con;
-
-        try {
-            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
-            System.out.println("Connection Successfully Established...!");
-
-            System.out.println("\nExecuting Query : \"SELECT * FROM EMPLOYEES\" ");
-            Statement query = con.createStatement();
-            ResultSet result = query.executeQuery("SELECT * FROM EMPLOYEES");
-
-            int row = 1;
-            while(result.next()) {
-                System.out.println("Row " + row++ + " ==> " +
-                        result.getInt(1) + ", " +
-                        result.getString(2) + ", " +
-                        result.getString(3) + ", " +
-                        result.getString(4) + ", " +
-                        result.getString(5) + ", " +
-                        result.getDate(6) + " "
-                        );
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-
+        Oracle db = new Oracle();
 
 
     }
--- src/database/Oracle.java
+++ src/database/Oracle.java
@@ -1,6 +1,46 @@
 package database;
 
+// imports required for the connection to be established with the server
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Oracle {
 
-    
+    // Attributes
+    // Establishing a connection
+    // con here is the object that stores the established connection details
+    private static Connection con;
+
+    /*Constructor*/
+    public Oracle() {
+        initDb();
+    }
+
+    // Connection details
+    /* DATABASE INITIALIZER
+    * Database: in case of oracle : jdbc:oracle:thin
+    * address | hostname: the address to the server, in our case @localhost
+    * SID : XE (since we have express edition as the server)
+    * username : the username for connection, that will be 'hr'
+    * password : the password for connection, that will be 'hr'
+    * */
+    private void initDb() {
+        System.out.println("\nInitializing Oracle Database...");
+
+        try {
+            con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe",
+                    "hr",
+                    "hr"
+            );
+
+            System.out.println("Connection Established with Oracle Server => hostname: @localhost , PortNumber : 1521, with SID : xe");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
