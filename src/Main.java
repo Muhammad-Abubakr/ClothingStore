53c7c8af73e4e4b37572cd62cd528ab086197b5d
frostywolf <kmabs36@gmail.com>
2022-06-24 18:15:41 +0500

    No need to import from java.sql in tha Main class, now that the logic has been moved to ClothingStore/src/database/Oracle

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 // importing the required Classes for establishing connection with OracleDB
 import database.Oracle;
 
-import java.io.File;
-import java.sql.*;
-import java.time.Instant;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
