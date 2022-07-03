8fff8b98ee9b2116a382f0154acd6841e6b7d0d7
frostywolf <kmabs36@gmail.com>
2022-07-03 13:20:54 +0500

    Created Interface to share the connection with the classes who need it

--- src/database/ConnectionDistributor.java
+++ src/database/ConnectionDistributor.java
@@ -2,6 +2,7 @@ package database;
 
 import java.sql.Connection;
 
+// An Interface to distribute the shared Connection with the oracle database
 public interface ConnectionDistributor {
     // Connection Established
     Connection con = Oracle.initDb();
